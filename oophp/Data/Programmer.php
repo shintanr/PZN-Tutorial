ad3d1189ff0f7320fc2beb0decb573e8ae8a176a
ShintaNR <shintanurrohmah143@gmail.com>
2022-05-17 13:40:57 +0700

    change to make check type data of object

--- oophp/Data/Programmer.php
+++ oophp/Data/Programmer.php
@@ -24,5 +24,15 @@ class Company {
 }
 
 function sayHelloProgrammer(Programmer $programmer){
-    echo "Hello Programmer $programmer->name". PHP_EOL;
+    if ($programmer instanceof BackendProgrammer){
+        echo "Hello Backend Programmer $programmer->name" . PHP_EOL;
+    } else if ($programmer instanceof FrontendProgrammer){
+        echo "Hello Frontend Programmer $programmer->name" . PHP_EOL;
+    } else if ($programmer instanceof Programmer){
+        echo "Hello Programmer $programmer->name" . PHP_EOL;
+    }
+
+
+    //instance of dilakukan untuk pengecekan
+    // kalau tipe data bukan objek biasanya menggunakan is, misal if (isString)
 }
