223780c7c56efc2a92c0972859a8374397438524
ShintaNR <shintanurrohmah143@gmail.com>
2022-05-17 14:59:37 +0700

    add Getter and Setter

---
+++ oophp/GetterAndSetter.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "Data/Category.php";
+use Data\Category;
+
+$category = new Category();
+$category->setName("Handphone");
+$category->setExpensive(true);
+
+$category->setName("");
+echo "Name: {$category->getName()}". PHP_EOL;
+echo "Expensive: {$category->isExpensive()}". PHP_EOL;
